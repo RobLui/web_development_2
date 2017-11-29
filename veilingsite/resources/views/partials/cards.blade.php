0e8c0684f3387dabe9bbdf7c30e592dae4bb4490
Robbert <robbertluit@hotmail.com>
2017-11-29 03:15:04 +0100

    add elements & gnite

--- veilingsite/resources/views/partials/cards.blade.php
+++ veilingsite/resources/views/partials/cards.blade.php
@@ -4,9 +4,24 @@
             <div class="col-md-12 margin-top-2">
                 <h3 class="midnight-blue">How does it work?</h3>
             </div>
-            <div class="auto cell"><h5>Sign up!</h5></div>
-            <div class="auto cell"><h5>Make deals</h5></div>
-            <div class="auto cell"><h5>Everyone happy!</h5></div>
+            <div class="auto cell">
+                <div class="col-md-12">
+                    <img class="img-responsive" src="{{ asset('images/elements/kleur.png') }}" alt="happy">
+                </div>
+                <h5>Sign up!</h5>
+            </div>
+            <div class="auto cell">
+                <div class="col-md-12">
+                    <img class="img-responsive" src="{{ asset('images/elements/vink.png') }}" alt="happy">
+                </div>
+                <h5>Make deals</h5>
+            </div>
+            <div class="auto cell">
+                <div class="col-md-12">
+                    <img class="img-responsive" src="{{ asset('images/elements/happy.png') }}" alt="happy">
+                </div>
+                <h5>Everyone happy!</h5>
+            </div>
         </div>
     </div>
 </div>

4822018f4e9fefc2da0d9f1804d13fd507e49d47
Robbert <robbertluit@hotmail.com>
2017-11-30 20:30:15 +0100

    update no-carous mockup

--- veilingsite/resources/views/partials/_overview.blade.php
+++ veilingsite/resources/views/partials/_overview.blade.php
@@ -53,6 +53,199 @@
                     <button class="btn background-light-grey margin-top-2"><span class="midnight-blue">I OWN THIS ARTWORK ></span></button>
                 </td>
             </tr>
+            <tr>
+                <td>
+                    <span class="black">Search:</span>
+                    <span class="light-dark-grey">
+                        Mona Lisa, Leonardo Da Vinci
+                    </span>
+                    <br>
+
+                    <span class="black">Description:</span>
+                    <span class="light-dark-grey">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    </span>
+
+                    <br>
+
+                    <span class="black">Posted:</span>
+                    <span class="light-dark-grey">
+                        5 July 2013
+                    </span>
+                </td>
+                <td>
+                    <button class="btn background-light-grey margin-top-2"><span class="midnight-blue">I OWN THIS ARTWORK ></span></button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span class="black">Search:</span>
+                    <span class="light-dark-grey">
+                        Mona Lisa, Leonardo Da Vinci
+                    </span>
+                    <br>
+
+                    <span class="black">Description:</span>
+                    <span class="light-dark-grey">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    </span>
+
+                    <br>
+
+                    <span class="black">Posted:</span>
+                    <span class="light-dark-grey">
+                        5 July 2013
+                    </span>
+                </td>
+                <td>
+                    <button class="btn background-light-grey margin-top-2"><span class="midnight-blue">I OWN THIS ARTWORK ></span></button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span class="black">Search:</span>
+                    <span class="light-dark-grey">
+                        Mona Lisa, Leonardo Da Vinci
+                    </span>
+                    <br>
+
+                    <span class="black">Description:</span>
+                    <span class="light-dark-grey">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    </span>
+
+                    <br>
+
+                    <span class="black">Posted:</span>
+                    <span class="light-dark-grey">
+                        5 July 2013
+                    </span>
+                </td>
+                <td>
+                    <button class="btn background-light-grey margin-top-2"><span class="midnight-blue">I OWN THIS ARTWORK ></span></button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span class="black">Search:</span>
+                    <span class="light-dark-grey">
+                        Mona Lisa, Leonardo Da Vinci
+                    </span>
+                    <br>
+
+                    <span class="black">Description:</span>
+                    <span class="light-dark-grey">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    </span>
+
+                    <br>
+
+                    <span class="black">Posted:</span>
+                    <span class="light-dark-grey">
+                        5 July 2013
+                    </span>
+                </td>
+                <td>
+                    <button class="btn background-light-grey margin-top-2"><span class="midnight-blue">I OWN THIS ARTWORK ></span></button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span class="black">Search:</span>
+                    <span class="light-dark-grey">
+                        Mona Lisa, Leonardo Da Vinci
+                    </span>
+                    <br>
+
+                    <span class="black">Description:</span>
+                    <span class="light-dark-grey">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    </span>
+
+                    <br>
+
+                    <span class="black">Posted:</span>
+                    <span class="light-dark-grey">
+                        5 July 2013
+                    </span>
+                </td>
+                <td>
+                    <button class="btn background-light-grey margin-top-2"><span class="midnight-blue">I OWN THIS ARTWORK ></span></button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span class="black">Search:</span>
+                    <span class="light-dark-grey">
+                        Mona Lisa, Leonardo Da Vinci
+                    </span>
+                    <br>
+
+                    <span class="black">Description:</span>
+                    <span class="light-dark-grey">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    </span>
+
+                    <br>
+
+                    <span class="black">Posted:</span>
+                    <span class="light-dark-grey">
+                        5 July 2013
+                    </span>
+                </td>
+                <td>
+                    <button class="btn background-light-grey margin-top-2"><span class="midnight-blue">I OWN THIS ARTWORK ></span></button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span class="black">Search:</span>
+                    <span class="light-dark-grey">
+                        Mona Lisa, Leonardo Da Vinci
+                    </span>
+                    <br>
+
+                    <span class="black">Description:</span>
+                    <span class="light-dark-grey">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    </span>
+
+                    <br>
+
+                    <span class="black">Posted:</span>
+                    <span class="light-dark-grey">
+                        5 July 2013
+                    </span>
+                </td>
+                <td>
+                    <button class="btn background-light-grey margin-top-2"><span class="midnight-blue">I OWN THIS ARTWORK ></span></button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span class="black">Search:</span>
+                    <span class="light-dark-grey">
+                        Mona Lisa, Leonardo Da Vinci
+                    </span>
+                    <br>
+
+                    <span class="black">Description:</span>
+                    <span class="light-dark-grey">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    </span>
+
+                    <br>
+
+                    <span class="black">Posted:</span>
+                    <span class="light-dark-grey">
+                        5 July 2013
+                    </span>
+                </td>
+                <td>
+                    <button class="btn background-light-grey margin-top-2"><span class="midnight-blue">I OWN THIS ARTWORK ></span></button>
+                </td>
+            </tr>
+        
 
         </tbody>
     </table>
--- veilingsite/resources/views/partials/no-carousel.blade.php
+++ veilingsite/resources/views/partials/no-carousel.blade.php
@@ -1,1 +1,3 @@
 <img src="{{ asset("images/mockups/platenspeler.png") }}" class="img full-width" alt="Responsive image">
+{{--<div class="lead">text</div>--}}
+

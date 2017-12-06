b91d77d9772ade8c9f2b335093aeeb5ca1480bb5
Robbert <robbertluit@hotmail.com>
2017-12-06 19:28:25 +0100

    add standard seo tags into view

---
+++ veilingsite/resources/views/standard-seo-tags.blade.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+    
+    <!-- META -->
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="Robbert Luit" />
+    <meta name="publisher" content="Robbert Luit" />
+    <meta name="robots" content="all" />
+    
+    <!-- TWITTER -->
+    <meta name="twitter:card" property="twitter:card" content="" />
+    <meta name="twitter:title" property="twitter:title" content="" />
+    <meta name="twitter:description" property="twitter:description" content="" />
+    <meta name="twitter:image" property="twitter:image" content="" />
+    <meta name="twitter:url" property="twitter:url" content="" />
+    
+    <!-- FACEBOOK  -->
+    <meta property="og:site_name" content="" />
+    <meta name="og:title" property="og:title" content="" />
+    <meta name="og:type" property="og:type" content="" />
+    <meta name="og:url" property="og:url" content="" />
+    <meta name="og:description" property="og:description" content="" />
+    <meta property="og:image" content="" />
+    <meta name="og:sitename" property="og:sitename" content="" />
+    
+    <!-- .ICO & VIEWPORT -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="icon" type="image/png" href="#favicon.png.hierzo" />
+  </head>
+  <body>
+    
+  </body>
+</html>  

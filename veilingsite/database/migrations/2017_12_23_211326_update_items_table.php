290246b98c8d290ddf1c15548c7318eeefa67ac6
Robbert <robbertluit@hotmail.com>
2017-12-23 22:22:42 +0100

    update migration table + foreigns + update deployment doc

---
+++ veilingsite/database/migrations/2017_12_23_211326_update_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('items', function (Blueprint $table) {
+
+            // USER
+            $table->foreign('fk_user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            // WATCHLIST
+            $table->foreign('fk_watchlist_id')
+                ->references('id')->on('watchlists')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

9cf277e307d84400cab77ae09740dba7f2815982
Robbert <robbertluit@hotmail.com>
2017-12-30 22:47:03 +0100

    finish detail

--- veilingsite/resources/views/partials/detail_description.blade.php
+++ veilingsite/resources/views/partials/detail_description.blade.php
@@ -1,22 +1,31 @@
-    <div class="col-sm-8 margin-bottom-2 margin-top-1">
+<div class="col-sm-12">
+    <div class="col-sm-11 bordered">
 
-        <div class="col-sm-12 ">
-            <div class="col-sm-12 bordered">
-                <p class="active-bold margin-top-1">Description</p>
-            </div>
+        <div class="col-sm-9 margin-bottom-2 margin-top-1">
+            <p class="active-bold margin-top-1">Description</p>
+            <p class="margin-top-minus-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="active-bold margin-top-1">Condition</p>
+            <p class="margin-top-minus-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
 
-    </div>
-
-
-    <div class="col-sm-4 margin-top-1">
-
-        <div class="col-sm-12">
-
-            <div class="col-sm-9 bordered">
+        <div class="col-sm-3 margin-top-1">
+            <div class="col-sm-12">
                 <p class="active-bold margin-top-1">Artist</p>
+                <p class="margin-top-minus-1">Salvador Dali</p>
+                <p class="margin-top-minus-1">Spanish</p>
+                <p class="margin-top-minus-1">1904 - 1989</p>
+                <p class="active-bold margin-top-1">Dimensions</p>
+                <p class="margin-top-minus-1">10.43 x 10.43 x 10.44 cm</p>
+                <p class="active-bold margin-top-1">Color</p>
+                <p class="margin-top-minus-1">Bronze, patinated bronze and gold</p>
             </div>
-
+            <div class="col-sm-12">
+                <button class="padding-complete background-dark-grey white active-bold full-width">
+                    ASK A QUESTION
+                    ABOUT THIS AUCTION
+                </button>
+            </div>
         </div>
 
     </div>
+</div>

2254140fcdba9b8dfa4c8a34be418b9cca55dc0a
Robbert <robbertluit@hotmail.com>
2017-12-31 02:47:40 +0100

    finish correct search on home

--- veilingsite/resources/views/partials/images.blade.php
+++ veilingsite/resources/views/partials/images.blade.php
@@ -12,7 +12,7 @@
                                 <img src="{{ asset('images/mockups/geld.png') }}" alt="Money" title="money">
                                 <div class="ovrly"></div>
                                 <div class="buttons">
-                                    <a href="#" class="fa fa-search"></a>
+                                    <a href="#" class="fa fa-2x  fa-search"></a>
                                 </div>
                             </div>
 
@@ -20,16 +20,17 @@
                                     <img class="margin-top-1" src="{{ asset('images/mockups/vrouw.png') }}" alt="Women" title="women" ></p>
                                     <div class="ovrly hug-top-complete"></div>
                                     <div class="buttons">
-                                        <a href="#" class="fa fa-search"></a>
+                                        <a href="#" class="fa fa-2x  fa-search"></a>
                                     </div>
                                 </div>
                             </div>
+
                             <div class="col-md-8 col-sm-8 margin-bottom-2">
                                 <div class="imagebtn">
                                     <img src="{{ asset('images/mockups/parfum.png') }}" alt="Perfume" title="perfume">
                                     <div class="ovrly"></div>
                                     <div class="buttons">
-                                        <a href="#" class="fa fa-2x fa-search"></a>
+                                        <a href="#" class="fa fa-4x fa-search"></a>
                                     </div>
                                 </div>
                             </div>

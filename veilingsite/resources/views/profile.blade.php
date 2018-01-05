155291e41aef44a9b364ef19a040ad0bf49919cb
Robbert <robbertluit@hotmail.com>
2018-01-05 22:53:02 +0100

    update profile

--- veilingsite/resources/views/profile.blade.php
+++ veilingsite/resources/views/profile.blade.php
@@ -17,22 +17,49 @@
             <div class="col-md-12 col-sm-12">
                 <div class="margin-top-2 margin-left-1">
                     <a class="midnight-light-blue float-left margin-right-half" href="{{ route('home') }}" title="home">Home > </a>
-                    <p class="midnight-light-blue margin-right-half">Jasper Poppe ></p>
+                    <p class="midnight-light-blue margin-right-half bold">Jasper Poppe</p>
                 </div>
                 <div class="margin-top-1 margin-left-1">
-                    <h2 class="margin-bottom-half light-dark-grey">My auctions</h2>
+                    <h2 class="margin-bottom-half light-dark-grey">Profile</h2>
                 </div>
-                <div class="bordered">
-                    <h5 class="margin-top-half margin-left-1">Jasper Poppe</h5>
-                    <div class="list-group">
-                        <li class="list-unstyled margin-top-2 margin-left-2">
-                            <i class="fa fa-envelope" aria-hidden="true"></i>
-                            <a href="mailto:info@landorettiart.com" title="info@landorettiart.com" class="underlined midnight-light-blue">info@landorettiart.com</a>
-                        </li>
-                        <li class="list-unstyled margin-left-2">
-                            <i class="fa fa-phone" aria-hidden="true"></i>
-                            <a href="tel:+xx (0)x xxx xx xx" title="tel:+xx (0)x xxx xx xx" class="light-dark-grey">+xx (0)x xxx xx xx</a>
-                        </li>
+                <div class="col-sm-12 bordered">
+                    {{--LEFT--}}
+                    <div class="col-sm-3">
+                        <h4 class="block margin-top-half margin-left-half ">Jasper Poppe</h4>
+                        <div class="bordered-right">
+                            <li class="list-unstyled margin-top-2 margin-left-1">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                <a href="mailto:name@domain.com" title="info@landorettiart.com" class="underlined midnight-light-blue">name@domain.com</a>
+                            </li>
+                            <li class="list-unstyled margin-left-1">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                <a href="tel:+xx (0)x xxx xx xx" title="tel:+xx (0)x xxx xx xx" class="light-dark-grey">+xx (0)x xxx xx xx</a>
+                            </li>
+                            <li class="list-unstyled margin-top-1 margin-left-1">
+                                <p>
+                                    Straatnaam xxx
+                                    <br>
+                                    xxxx, Oostende
+                                </p>
+                            </li>
+                        </div>
+                    </div>
+                    {{--RIGHT--}}
+                    <div class="col-sm-3">
+                        <div class="margin-top-4 col-sm-12 margin-bottom-2">
+                            <li class="list-unstyled margin-top-1">
+                                <span class="midnight-blue">VAT-number</span>
+                            </li>
+                            <li class="list-unstyled">
+                                <span class="margin-left-1">xx xxx xx xx</span>
+                            </li>
+                            <li class="list-unstyled margin-top-1">
+                                <span class="midnight-blue">Account number</span>
+                            </li>
+                            <li class="list-unstyled">
+                                <span class="margin-left-1">xx xxx xx xx</span>
+                            </li>
+                        </div>
                     </div>
                 </div>
             </div>
